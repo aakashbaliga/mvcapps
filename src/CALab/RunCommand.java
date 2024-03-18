13ed0ffb293a720e3107520d611a7a24e4e1714e
Arav Dharmesh Panchmatia <136196166+BinaryAP@users.noreply.github.com>
2024-03-17 19:07:47 -0700

    Update RunCommand.java

--- src/CALab/RunCommand.java
+++ src/CALab/RunCommand.java
@@ -1,4 +1,18 @@
 package CALab;
 
-public class RunCommand {
+import mvc.*;
+
+public class RunCommand extends Command {
+
+  Grid myGrid;
+  int count;
+
+  public RunCommand(Grid grid, int ct) {
+    myGrid = grid;
+    count = ct;
+  }
+  
+  public void execute() {
+    myGrid.updateLoop(count);
+  }
 }
