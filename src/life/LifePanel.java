30787ec6d14c34dc65bec2f86324911a3a55e70a
Arav Dharmesh Panchmatia <136196166+BinaryAP@users.noreply.github.com>
2024-03-20 13:57:42 -0700

    Update LifePanel.java

--- src/life/LifePanel.java
+++ src/life/LifePanel.java
@@ -6,4 +6,10 @@ public class LifePanel extends GridPanel {
     public LifePanel(GridFactory factory) {
         super(factory);
     }
+
+    public static void main(String[] args) {
+        GridFactory f = new LifeFactory();
+        GridPanel p = new GridPanel(f);
+        p.show();
+    }
 }
